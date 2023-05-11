efb43ba65e641fc93315991cff8f1bcc9d951f04
vkuday <kudayvolkan@gmail.com>
2023-05-10 22:37:03 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Volkan Kuday on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
